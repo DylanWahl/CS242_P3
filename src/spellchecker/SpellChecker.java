5c08f7274565945c57cb09fbc0f367affd3c3d1b
Dylan-W3695 <wahl_d@lynchburg.edu>
2020-03-26 15:26:07 -0400

    first pass at editDistance

--- src/spellchecker/SpellChecker.java
+++ src/spellchecker/SpellChecker.java
@@ -1,5 +1,7 @@
 package spellchecker;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class SpellChecker {
@@ -17,20 +19,33 @@ public class SpellChecker {
 	}
 
 	public static int editDistance(String s1, String s2) {
-		int distance = 0;
-		
-
-		return distance;
+		int d = 0;
+		return distance(s1.toLowerCase(), s2.toLowerCase(), d);
 	}
-	
-	private int distance(String s1, String s2, int distance) {
+
+	private static int distance(String s1, String s2, int d) {
 		if (s1.length() == 0) {
-			distance = s2.length();
+			d += s2.length();
 		} else if (s2.length() == 0) {
-			distance = s1.length();
-		} else if (!s1.equalsIgnoreCase(s2)) {
-			
+			d += s1.length();
+		} else if (!s1.equals(s2)) {
+			char s1Last = s1.charAt(s1.length() - 1);
+			char s2Last = s2.charAt(s2.length() - 1);
+
+			if (s1Last == s2Last) {
+				d += distance(s1.substring(0, s1.length() - 1), s2.substring(0, s2.length() - 1), d);
+			} else {
+				ArrayList<Integer> RCValues = new ArrayList<Integer>(3);
+
+				RCValues.add(distance(s1.substring(0, s1.length() - 1), 
+						s2.substring(0, s2.length() - 1), d) + 1);
+				RCValues.add(distance(s1, s2.substring(0, s2.length() - 1), d));
+				RCValues.add(distance(s1.substring(0, s2.length() - 1), s2, d));
+				d += Collections.min(RCValues);
+
+			}
 		}
+		return d;
 	}
 
 	public static List<String> suggestWordsTest(String word, int maxEditDistance, List<String> lexicon) {
