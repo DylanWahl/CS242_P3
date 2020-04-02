985a4e083dac977b5613dd9b8a28105cf5ae708c
Dylan-W3695 <wahl_d@lynchburg.edu>
2020-04-02 16:29:57 -0400

    perfect editDistance and build tree first Draft

--- src/spellchecker/Milestone1.java
+++ src/spellchecker/Milestone1.java
@@ -6,13 +6,13 @@ public class Milestone1 {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Please enter your first word ('0' to exit)");
+		System.out.print("Please enter your first word ('0' to exit): ");
 		String firstWord = scanner.nextLine();
 		while(!firstWord.equals("0")) {
-			System.out.println("Please enter your second word");
+			System.out.print("Please enter your second word: ");
 			String secondWord = scanner.nextLine();
 			System.out.println(SpellChecker.editDistance(firstWord, secondWord));
-			System.out.println("Please enter your first word ('0' to exit)");
+			System.out.print("Please enter your first word ('0' to exit): ");
 			firstWord = scanner.nextLine();
 		}
 
--- src/spellchecker/SpellChecker.java
+++ src/spellchecker/SpellChecker.java
@@ -2,16 +2,49 @@ package spellchecker;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 public class SpellChecker {
 
-	public SpellChecker(List<String> list) {
+	List<String> lexicon;
+	Node root;
 
+	public SpellChecker(List<String> list) {
+		lexicon = list;
+		root = new Node();
+	}
+
+	private void buildTree() {
+
+		for (String s : lexicon) {
+			addWord(root, s, 0);
+		}
+	}
+	//base case........ 
+	private void addWord(Node root, String word, int edgeIndex) {
+		boolean edgeFound = false;
+		Node newRoot = null;
+		LinkedList<Node> rootChildren = root.getChildren();
+		
+		for (int i = 0; i < rootChildren.size() && edgeFound == false; i++) {
+			if (word.charAt(edgeIndex) == rootChildren.get(i).getEdge()) {
+				edgeFound = true;
+				newRoot = rootChildren.get(i);
+			}
+		}
+		if (!edgeFound) {
+			newRoot = new Node(word.charAt(edgeIndex), word.substring(0, edgeIndex++));
+			rootChildren.add(newRoot);
+			
+		}
+		addWord(newRoot, word, edgeIndex++);
 	}
 
 	public boolean spelledCorrectly(String word) {
-		return false;
+		boolean found = false;
+
+		return found;
 	}
 
 	public List<String> suggestWords(String word, int maxEditDistance) {
@@ -19,37 +52,76 @@ public class SpellChecker {
 	}
 
 	public static int editDistance(String s1, String s2) {
-		int d = 0;
-		
-		int[][] table  = new int [s1.length()+ 1][s2.length() + 1];
-		
+		s1 = s1.toLowerCase();
+		s2 = s2.toLowerCase();
+
+		int[][] table = new int[s1.length() + 1][s2.length() + 1];
+
 		table[0][0] = 0;
-		for(int i = 0; i <= s1.length(); i++) {
+		for (int i = 0; i <= s1.length(); i++) {
 			table[i][0] = i;
 		}
-		for(int j = 0; j <= s2.length(); j++) {
+		for (int j = 0; j <= s2.length(); j++) {
 			table[0][j] = j;
 		}
-		
-		for(int i = 1; i <= s1.length(); i++) {
-			for(int j = 1; j <= s2.length(); j++) {
+
+		for (int i = 1; i <= s1.length(); i++) {
+			for (int j = 1; j <= s2.length(); j++) {
 				ArrayList<Integer> RCValues = new ArrayList<Integer>(3);
 				int removeBothOffset = 1;
-				if(s1.charAt(i-1) == s2.charAt(j-1)) {
+				if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
 					removeBothOffset = 0;
 				}
-				
-				RCValues.add(table[i-1][j] + 1);
-				RCValues.add(table[i][j-1] + 1);
+
+				RCValues.add(table[i - 1][j] + 1);
+				RCValues.add(table[i][j - 1] + 1);
 				RCValues.add(table[i - 1][j - 1] + removeBothOffset);
 				table[i][j] = Collections.min(RCValues);
 			}
 		}
-		
+
 		return table[s1.length()][s2.length()];
 	}
 
 	public static List<String> suggestWordsTest(String word, int maxEditDistance, List<String> lexicon) {
 		return null;
 	}
+
+	private class Node {
+
+		private LinkedList<Node> children;
+		private String word;
+		private char edgeLetter;
+		private boolean isPartial;
+
+		Node() {
+		}
+
+		Node(char edgeLetter, String word) {
+			this.edgeLetter = edgeLetter;
+			word = word;
+			isPartial = lexicon.contains(word);
+		}
+
+		public void addChild(Node newChild) {
+			children.add(newChild);
+		}
+
+		public LinkedList<Node> getChildren() {
+			return children;
+		}
+
+		public boolean isPartial() {
+			return isPartial;
+		}
+
+		public String getData() {
+			return word;
+		}
+		public char getEdge() {
+			return edgeLetter;
+		}
+
+	}
+
 }
